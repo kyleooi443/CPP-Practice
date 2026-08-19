fef15c90313cecdd5d9a16689401ded6b235e3b0
Kyle Masilang <kyleqndrew@gmail.com>
2026-08-19 19:50:19 +0800

    Add files via upload

---
+++ notes.cpp
@@ -0,0 +1,34 @@
+#include <iostream>     //library for cin and cout
+#include <string>       // library for string
+
+
+using namespace std;    // to avoid using std:: 
+
+
+
+
+/* Variables
+
+int - stores integers (whole numbers), without decimals, such as 123 or -123
+
+double - stores floating point numbers, with decimals, such as 19.99 or -19.99
+
+char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+
+string - stores text, such as "Hello World". String values are surrounded by double quotes
+
+bool - stores values with two states: true or false
+
+*/
+
+// cout     //print
+// cin      //input
+
+//int       // integers
+//float     // decimal 6 to 7 decimal digits
+//double    // decimal up to 17 decimal digits
+//char      // single character
+//bool      // boolean, true or false
+//string    // std::string, string
+
+//auto      // automatically chooses permanent fixed data type after line
---
+++ test.cpp
@@ -0,0 +1,227 @@
+#include <iostream> //library for cin and cout
+#include <string> //include the string library
+
+
+using namespace std; // to avoid using std:: 
+
+
+/* int main() { // set main as a function 
+string name; // set 'name' as string
+  cout << "What's your name?\n";
+  cin >> name;
+  cout << "Hello, " << name << "!\n";
+  return 0;
+}*/
+
+/*
+int main () { 
+    string name; 
+        cout << "What's your name?\n";
+        cin >> name;
+        cout << "Hello, " << name << "!" << endl;
+return 0;
+}
+*/
+
+
+/*
+int main () {
+    cout << "Hello, I'm Kyle!\n";
+    cout << "Hello, I'm Jed!\n";
+    cout << "Hello, I'm Mark!\n";
+return 0;
+*/
+
+/*
+int main () {
+
+    int x, y;
+
+    cout << 'Whats x? '; cin >> x;
+    cout << "Whats y?"; cin >> y;
+
+    cout << x * y << '\n';
+    return 0;
+}
+*/
+
+/*
+int main () {
+
+double pi = 3.1418;
+int x = 5;
+
+cout << x / pi << '\n';
+
+}
+*/
+
+/* 
+int main () {
+
+    string name;
+    int age, grade;
+
+    cout << "Whats your name? "; cin >> name;
+
+    cout << "Whats your age? "; cin >> age;
+
+    cout <<  "Whats your grade level? "; cin >> grade;
+
+
+    cout << "Hello, " <<  name << "!, " << age << " years old, " << "grade " << grade;
+    return 0;
+}
+*/
+
+/* Constants 
+int main () {
+
+const int num = 10; 
+num = 4;
+
+
+}
+*/
+
+/*
+int main () { 
+
+    int length, width, area;
+
+    cout << "Whats the length? "; cin >> length;
+    cout << "Whats the width? "; cin >> width;
+
+    area = length * width;
+
+    cout << "The area of the rectangle is " << area;
+
+return 0;
+}
+*/
+/*
+int main () { 
+    int studentID = 15;
+    int studentAge = 18;
+    double studentFee = 72.34;
+    char studentGrade = 'B';
+
+    cout << "Student ID: " << studentID << "\n";
+    cout << "Student Age: " << studentAge << "\n";
+    cout << "Student Fee: " << studentFee << "\n";
+    cout << "Student Grade: " << studentGrade << "\n";
+
+
+return 0;
+}
+*/
+
+
+
+
+/*
+
+
+
+int main () { 
+    float f1 = 35e3; // 'e' indicates the power of 10
+    double d1 = 12E4;
+    cout << f1;
+    cout << d1;
+return 0;
+
+}
+
+*/
+
+
+/*
+int main () {
+
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+
+    cout << isCodingFun;
+    cout << isFishTasty;
+
+    // When the value is returned, true = 1, false = 0.
+
+return 0;
+}
+
+*/
+
+
+/*
+int main () { 
+
+    char myGrade = 'B';
+    cout << myGrade; 
+
+    char a = 65, b = 66, c = 67;
+    cout << a;
+    cout << b;
+    cout << c;
+
+return 0;    
+}
+*/
+
+/*
+int main () {
+
+    string greeting = "Hello, World!";
+    cout << greeting;
+
+return 0;
+
+}
+*/
+
+/*
+int main () {
+
+    int items = 50;
+    double cost_per_item = 9.99;
+    double total_cost = items * cost_per_item;
+    char currency = '$';
+
+    cout << "Number of Items: ";
+    cout << "Cost of Item: " << currency << cost_per_item << '\n';
+    cout << "Total Cost: " << currency << total_cost << '\n';
+
+return 0;
+
+}
+*/
+
+int main () {
+
+    int x = 10;
+    int y = 3;
+
+
+    cout << (x + y) << "\n"; // 13
+    cout << (x - y) << "\n"; // 7
+    cout << (x * y) << "\n"; // 30
+    cout << (x / y) << "\n"; // 3 (integer division)
+    cout << (x % y) << "\n"; // 1
+
+    int z = 5;
+    ++z;                    // Increment, increase value
+    cout << z << "\n"; 
+    --z; --z;                    // Decrement, decrease value
+    cout << z << "\n";
+
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
